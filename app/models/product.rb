496f1cf34cd68762c686eb939a888bd20cc43eed
Warren Bain <warren@ninefold.com>
2013-11-02 21:14:13 +1100

    Iteration B1

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,11 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
