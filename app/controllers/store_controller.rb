b9797bea64a0963b5f8ec3c888caf3972c87309d
Warren Bain <warren@ninefold.com>
2013-11-03 15:23:29 +1100

    Completed Task D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,12 @@ class StoreController < ApplicationController
   def index
     @time = Time.now.strftime("%F %H:%M")
     @products = Product.order(:title)
+
+    @counter = if session[:counter].nil?
+
+                 session[:counter] = 0
+               else
+                 session[:counter] += 1
+               end
   end
 end
